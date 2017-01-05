e02634da87a186510b28bda38cab0dda1ecd7b9a
pale-passenger <pale_passenger@yahoo.com>
2017-01-05 09:39:54 +0500

    Update setInterval.js

--- setInterval.js
+++ setInterval.js
@@ -1,4 +1,4 @@
 var http = require("http");
 setInterval(function() {
-    http.get("http://cloud-torrent-01.herokuapp.com");
+    http.get("https://cloud-torrent-01.herokuapp.com");
 }, 300000); // every 5 minutes (300000)
